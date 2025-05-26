53f4d59b7f27b9e164744f7bdf06093d13bf58ad
hacker77189 <aminhaj774@gmail.com>
2025-05-26 16:16:42 +0000

    convesrion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "33abc" 
+
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueNumber = Number(score)
+console.log(valueNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+let isLoggedIn = "minhaj"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "minhaj" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
