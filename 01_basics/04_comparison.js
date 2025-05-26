ca6e16bec210be6444c81b4bd210cef0db4152fa
hacker77189 <aminhaj774@gmail.com>
2025-05-26 16:49:51 +0000

    comp

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,14 @@
+console.log("2" > 1) //Don't Use
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+
+// === => used for strict check 
+
+console.log("2" === 2);
